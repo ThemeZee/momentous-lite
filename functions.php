9f1bef4319f858e4cb5396fa2b548c24b6b16adc
Thomas W <netzberufler@googlemail.com>
2015-08-27 15:07:27 +0200

    improved phrasing of all translate-able texts

--- functions.php
+++ functions.php
@@ -157,7 +157,7 @@ function momentous_register_sidebars() {
 	register_sidebar( array(
 		'name' => __( 'Sidebar', 'momentous-lite'),
 		'id' => 'sidebar',
-		'description' => __( 'Appears on posts and pages except front page and fullwidth template.', 'momentous-lite'),
+		'description' => __( 'Appears on posts and pages except Magazine Homepage and Fullwidth template.', 'momentous-lite'),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widgettitle"><span>',
