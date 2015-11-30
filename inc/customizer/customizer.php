c668aa0d9ca447bbcaa0afe7e048b5bca0054fcb
Thomas W <netzberufler@googlemail.com>
2015-11-30 18:28:20 +0100

    added custom header image settings

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -49,7 +49,41 @@ function momentous_customize_register_options( $wp_customize ) {
         'section'  => 'title_tagline',
         'settings' => 'momentous_theme_options[header_tagline]',
         'type'     => 'checkbox',
-		'priority' => 99
+		'priority' => 10
+		)
+	);
+	
+	// Add Header Image Link
+	$wp_customize->add_setting( 'momentous_theme_options[custom_header_link]', array(
+        'default'           => '',
+		'type'           	=> 'option',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'esc_url'
+		)
+	);
+    $wp_customize->add_control( 'momentous_control_custom_header_link', array(
+        'label'    => esc_html__( 'Header Image Link', 'momentous-lite' ),
+        'section'  => 'header_image',
+        'settings' => 'momentous_theme_options[custom_header_link]',
+        'type'     => 'url',
+		'priority' => 10
+		)
+	);
+	
+	// Add Custom Header Hide Checkbox
+	$wp_customize->add_setting( 'momentous_theme_options[custom_header_hide]', array(
+        'default'           => false,
+		'type'           	=> 'option',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'momentous_sanitize_checkbox'
+		)
+	);
+    $wp_customize->add_control( 'momentous_control_custom_header_hide', array(
+        'label'    => esc_html__( 'Hide header image on front page', 'momentous-lite' ),
+        'section'  => 'header_image',
+        'settings' => 'momentous_theme_options[custom_header_hide]',
+        'type'     => 'checkbox',
+		'priority' => 15
 		)
 	);
 	
