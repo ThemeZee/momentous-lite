7f4a06b3fa5a78e299c87ec68526d088d0e8600b
Thomas W <netzberufler@googlemail.com>
2015-12-21 16:47:36 +0100

    added support for Related Posts plugin

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -237,6 +237,25 @@ if ( ! function_exists( 'momentous_display_postinfo_single' ) ) :
 	
 endif;
 
+
+// Display Momentous Lite plugin
+if ( ! function_exists( 'momentous_display_related_posts' ) ):
+	
+	function momentous_display_related_posts() { 
+		
+		if ( function_exists( 'themezee_related_posts' ) ) {
+
+			themezee_related_posts( array( 
+				'class' => 'related-posts widget clearfix',
+				'before_title' => '<h2 class="widgettitle related-posts-title"><span>',
+				'after_title' => '</span></h2>'
+			) );
+			
+		}
+	}
+	
+endif;
+
 	
 // Display Content Pagination
 if ( ! function_exists( 'momentous_display_pagination' ) ):
